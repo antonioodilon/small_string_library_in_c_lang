2e65c87f69deb098959743484b9b771155a9a6fe
Antonio de Odilon <antonioodilonbrito@outlook.com>
2023-09-19 17:24:46 -0300

    Second commit. Removed certain unnecessary lines of code for the program.

--- array_char_functions.h
+++ array_char_functions.h
@@ -27,7 +27,6 @@ size_t n_to_the_power_of_k(size_t n, size_t k)
 
 int deep_copy_array_chars(uint8_t * p_array_chars_dest, uint8_t * p_array_chars_source, size_t len_source)
 {
-	//p_array_chars_dest = (uint8_t *)realloc(p_array_chars_dest, ((len_source + 1) * sizeof(uint8_t))); // 1 for the '\0' character
 	uint8_t * temporary = (uint8_t *)malloc((len_source + 1) * sizeof(uint8_t));
 	if (temporary == NULL)
 	{
@@ -37,7 +36,6 @@ int deep_copy_array_chars(uint8_t * p_array_chars_dest, uint8_t * p_array_chars_
 	size_t i = 0;
 	while (i < len_source)
 	{
-		//p_array_chars_dest[i] = p_array_chars_source[i];
         temporary[i] = p_array_chars_source[i];
 		++i;
 	}
@@ -52,20 +50,13 @@ int deep_copy_array_chars(uint8_t * p_array_chars_dest, uint8_t * p_array_chars_
 uint8_t * transfer_array_chars(uint8_t * p_array_chars_source, size_t len_source)
 {
     uint8_t * array_chars_to_return = (uint8_t *)malloc((len_source + 1) * sizeof(uint8_t));
-	/*if (array_chars_to_return == NULL)
-	{
-		return -1;
-	}*/
 
 	size_t i = 0;
 	while (i < len_source)
 	{
-		//p_array_chars_dest[i] = p_array_chars_source[i];
         array_chars_to_return[i] = p_array_chars_source[i];
 		++i;
 	}
-	
-	//p_array_chars_dest[i] = '\0';
     array_chars_to_return[i] = '\0';
 
     return array_chars_to_return;
@@ -74,15 +65,10 @@ uint8_t * transfer_array_chars(uint8_t * p_array_chars_source, size_t len_source
 uint8_t * transfer_array_chars_resize(uint8_t * p_array_chars_source, size_t len_source, size_t amount_bytes)
 {
     uint8_t * array_chars_to_return = (uint8_t *)malloc(amount_bytes);
-	/*if (array_chars_to_return == NULL)
-	{
-		return -1;
-	}*/
 
 	size_t i = 0;
 	while (i < len_source)
 	{
-		//p_array_chars_dest[i] = p_array_chars_source[i];
         array_chars_to_return[i] = p_array_chars_source[i];
 		++i;
 	}
@@ -95,8 +81,6 @@ uint8_t * transfer_array_chars_resize(uint8_t * p_array_chars_source, size_t len
 
 uint64_t concat_elements_in_array_of_chars(uint8_t * array_param, size_t length, bool do_concatenate_in_little_endian)
 {
-    // Write some documentation!!
-    // The return value only makes sense if it is printed in hexadecimal format
     uint64_t result = 0;
     uint64_t result_to_return = 0;
     uint64_t val_256_in_hex = 0x100;
@@ -107,7 +91,6 @@ uint64_t concat_elements_in_array_of_chars(uint8_t * array_param, size_t length,
         {
             uint64_t power_of_256 = 0;
             uint8_t array_param_i_converted = (uint8_t)array_param[i - 1];
-            //printf("%x - %u - %c\n", array_param_i_converted, array_param_i_converted, array_param_i_converted);
 
             if ((i - 1) == 0)
             {
@@ -118,10 +101,8 @@ uint64_t concat_elements_in_array_of_chars(uint8_t * array_param, size_t length,
             }
             
             result = (uint64_t)array_param_i_converted * power_of_256;
-            //printf("%lu\n", result);
 
             result_to_return += result;
-            //printf("%lu\n", result_to_return);
         }
     } else
     {
@@ -160,10 +141,8 @@ int push_char_into_char_array(uint8_t * p_array_param, uint8_t char_to_push, siz
 
 uint8_t * transform_uinteger_into_array_chars(uint64_t integer_value, uint64_t len_return_array, bool make_array_little_endian)
 {
-    // Write some documentation!!
     size_t amount_bytes_return_array = 1;
-    uint8_t * p_array_to_return = (uint8_t *)malloc(amount_bytes_return_array * sizeof(uint8_t)); // Only space for the null character,
-    // since chars will be added to it later on in the function
+    uint8_t * p_array_to_return = (uint8_t *)malloc(amount_bytes_return_array * sizeof(uint8_t));
     uint64_t val_256_in_hex = 0x100;
 
     for (size_t i = (len_return_array - 1); i >= 0; --i)
@@ -182,20 +161,17 @@ uint8_t * transform_uinteger_into_array_chars(uint64_t integer_value, uint64_t l
             power_of_256 = n_to_the_power_of_k(val_256_in_hex, i);
         }
 
-        temp_int_val /= power_of_256; // Integer division: if integer_value is, for example, 0x215888,
-        // and power_of_256 is 0x10000, then temp_int_val will be 21
+        temp_int_val /= power_of_256;
 
-        ++amount_bytes_return_array; // At this point amount_bytes_return_array is 2
+        ++amount_bytes_return_array;
         size_t index = amount_bytes_return_array - 2;
         uint8_t char_temp_int_val = (uint8_t)temp_int_val;
         push_char_into_char_array(p_array_to_return, char_temp_int_val, amount_bytes_return_array, index);
         uint64_t temp_temp_int_val = temp_int_val;
 
-        temp_int_val *= power_of_256; // Now when we multiply temp_int_val by 0x10000 again, temp_int_val
-        // will be 0x210000 (5888 will have been excluded)
+        temp_int_val *= power_of_256;
         if (temp_temp_int_val > 0)
             integer_value -= temp_int_val;
-        // All good! Now we need to store temp_int_val into an array
 
         if (i == 0)
             break;
@@ -203,7 +179,6 @@ uint8_t * transform_uinteger_into_array_chars(uint64_t integer_value, uint64_t l
 
     if (make_array_little_endian)
     {
-        // Exchange the position of the elements
         size_t index_end_of_the_array = len_return_array - 1;
         for (size_t i = 0; i < len_return_array;)
         {
--- main.c
+++ main.c
@@ -5,13 +5,6 @@
 #include <time.h>
 #include "string_struct_and_functions.h"
 
-typedef struct
-{
-    StringOfChars * buffer_strings;
-    size_t length;
-    size_t capacity;
-} VectorStrings;
-
 void continue_program(void)
 {
     StringOfChars input = input_growable_string();
@@ -36,21 +29,6 @@ void free_strings_in_vector(StringOfChars * vector_strings, size_t length)
 
 int main(void)
 {
-    
-    /*size_t len_1 = calc_arraychars_length("ABCDJSDKSDDFSJ");
-    StringOfChars string1 = new_string("ABCDJSDKSDDFSJ", len_1);
-    printf("%s\n", string1.buffer);
-    size_t len_2 = calc_arraychars_length("ZNKVISHNDFSIDFJSDJFG");
-    StringOfChars string2 = new_string("ZNKVISHNDFSIDFJSDJFG", len_2);
-    printf("%s\n", string2.buffer);
-
-    concatenate_strings(&string1, &string2);
-    printf("%s\n", string1.buffer);
-    free(string1.buffer);
-    free(string2.buffer);*/
-
-    //printf("================= PART 1 OF THE PROGRAM =================\n");
-
     int amount_strings;
     printf("(Please enter only numbers in order not to crash the program)\nHow many text strings would you like to input? ");
     scanf("%d", &amount_strings);
@@ -58,18 +36,11 @@ int main(void)
     StringOfChars * buffer_strings = (StringOfChars*)malloc(amount_strings * sizeof(StringOfChars));
 
     int i = 0;
-    //bool continue_loop = false;
-
-    //while (i <= amount_strings)
     while (i < amount_strings)
     {
-        //if (i >= 1)
-        //{
             printf("Enter the string number %d (any amount of characters should work!): ", i + 1);
             buffer_strings[i] = input_growable_string();
-        //}
         ++i;
-        //continue_loop = true;
     }
 
     printf("These were the strings you printed: \n");
@@ -77,14 +48,10 @@ int main(void)
 
     char first_string_to_concat[] = "The fully concatenated string: ";
     size_t length = calc_arraychars_length(first_string_to_concat);
-    //printf("%s, %d\n", first_string_to_concat, length);
 
     StringOfChars concatenated_string = new_string(first_string_to_concat, length);
-    //printf("%s\n", concatenated_string.buffer);
 
-    //StringOfChars concatenated_string = construct_string();
     printf("Now let us concatenate them all (as well as insert some text of our own!). Press any key: \n");
-    //StringOfChars input_1 = input_growable_string();
     continue_program();
     for (size_t j = 0; j < amount_strings; ++j)
     {
@@ -97,24 +64,17 @@ int main(void)
             printf("There was an error while concatenating the strings.");
         }
     }
-
-    //printf("The fully concatenated string with all the inputs you provided is %s\n", concatenated_string.buffer);
+    
     printf("%s\n", concatenated_string.buffer);
 
-    //free(input_1.buffer);
-
-    //printf("\n================= PART 2 OF THE PROGRAM =================\n");
     printf("Moreover, the characters in the concatenated string all have ASCII values that can be expressed in hexadecimal and decimal.\n");
     printf("Let's have a look. Press any key: ");
     continue_program();
-    //StringOfChars input_2 = input_growable_string();
+    
     print_str_skip_lines(&concatenated_string);
-    //free(input_2.buffer);
 
     printf("\nAs you know, 64-bit operating systems can express numbers that are up to 8 bytes long. And since characters are actually numbers,"
     " let us choose 8 random characters from the concatenated string! Press any key: ");
-    //StringOfChars input_3 = input_growable_string();
-    //free(input_3.buffer);
     continue_program();
 
     srand(time(NULL));
@@ -126,14 +86,10 @@ int main(void)
         array_with_rand_values[j] = concatenated_string.buffer[random_value];
         printf("Random value (INDEX POSITION): %d | CHAR: %c - HEX: %x - DECIMAL: %u\n", random_value, array_with_rand_values[j], array_with_rand_values[j], array_with_rand_values[j]);
     }
-
-    // Let's see what those values are:
     
     printf("\nWe can also transform this new array of characters into a 64-bit number of its own by concatenating these characters with the help of the "
     "'concat_elements_in_array_of_chars' function in our little library inside the 'array_char_functions.h' header file!\n");
     printf("Press any key: ");
-    //StringOfChars input_4 = input_growable_string();
-    //free(input_4.buffer);
     continue_program();
 
     uint64_t resulting_64bit_num = concat_elements_in_array_of_chars(array_with_rand_values, RAND_ARRAY_LEN, false);
@@ -146,8 +102,6 @@ int main(void)
     " processors most of our computers use. Thankfully, our 'concat_elements_in_array_of_chars' function gives us the option concatenate our characters in little"
     " endian format (which corresponds to the architecture of the x86 Intel-based processors)!\n");
     printf("Press any key: ");
-    //StringOfChars input_5 = input_growable_string();
-    //free(input_5.buffer);
     continue_program();
 
     uint64_t resulting_64bit_little_endian = concat_elements_in_array_of_chars(array_with_rand_values, RAND_ARRAY_LEN, true);
@@ -156,8 +110,6 @@ int main(void)
     printf("However, we may also want to do the contrary, that is, get a 64-bit number and transform that into an array of chars by separating its bytes!\n");
     printf("So let's get the address of the first value inside our concatenated string and use that!\n");
     printf("Press any key: ");
-    //StringOfChars input_6 = input_growable_string();
-    //free(input_6.buffer);
     continue_program();
 
     uint64_t address_first_char = (uint64_t)&concatenated_string.buffer[0];
@@ -175,8 +127,6 @@ int main(void)
     print_array_chars_skip_line(array_chars_little_endian, amount_bytes_64bit_nums);
     printf("\n(Notice how these characters don't necessarily have a valid ASCII representation in human-readable letters!)");
 
-    //printf("Press any key: ");
-    //continue_program();
     printf("\n================ This is the end of our program! Thank you for checking it out! ================\n");
     printf("Press any key to end it: ");
     continue_program();
